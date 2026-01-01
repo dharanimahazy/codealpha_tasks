462747d1a5151507671443d9c39174c37a3a73ab
dharanimahazy <dharanimahazy@gmail.com>
2026-01-01 17:46:31 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+    private String name;
+    private int mark;
+
+    public Student(String name, int mark) {
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+}
