e14e960ff6366578117b751fe5a9a88970df9ad1
dharanimahazy <dharanimahazy@gmail.com>
2026-01-01 18:18:35 +0530

    Create Room.java

---
+++ Task4_HotelReservationSystem/Room.java
@@ -0,0 +1,32 @@
+public class Room {
+
+    private int roomNumber;
+    private String category;
+    private boolean available;
+
+    public Room(int roomNumber, String category) {
+        this.roomNumber = roomNumber;
+        this.category = category;
+        this.available = true;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void bookRoom() {
+        available = false;
+    }
+
+    public void freeRoom() {
+        available = true;
+    }
+}
